57c108056744714f8ff56ffd521b347157c1dc32
ilkinhuseynov1990 <49427513+ilkinhuseynov1990@users.noreply.github.com>
2019-07-07 20:24:45 +0400

    Homework

---
+++ Mylastindex and Simple number/Homework/Homework/Extension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Homework
+{
+		public static class Extension
+		{
+			public static int MyLastindexof(this string str, string sim)
+			{
+				int pos = -1;
+
+				for (int i = 0; i < str.Length; i++)
+				{
+					if (str[i] == sim[0])
+					{
+						pos = i;
+						pos++;
+					}
+				}
+				return pos;
+			}
+
+
+
+			public static void SimpleNumber(this int num)
+			{
+				string result = "";
+
+
+				if (num % 2 != 0)
+				{
+					result = "Sade ededdir";
+				}
+				else
+				{
+					result = "Murekkeb ededdir";
+				}
+
+				Console.WriteLine(result);
+
+
+			}
+		}
+	
+}
---
+++ Mylastindex and Simple number/Homework/Homework/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Homework
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string Name = "ilkin";
+
+			Console.WriteLine(Name.MyLastindexof("i"));
+
+			Console.WriteLine(Name.MyLastindexof("o"));
+
+
+			int number = 12;
+
+			number.SimpleNumber();
+
+			int number1 = 9;
+
+			number1.SimpleNumber();
+
+		}
+	}
+}
